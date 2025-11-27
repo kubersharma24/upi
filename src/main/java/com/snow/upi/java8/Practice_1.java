58b72b916e33e9f223472c7d7f597d7102020a84
kubersharma24 <kubersharma562@gmail.com>
2025-11-27 14:12:11 +0530

    java 8 practice

---
+++ src/main/java/com/snow/upi/java8/Practice_1.java
@@ -0,0 +1,35 @@
+package com.snow.upi.java8;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Practice_1 {
+	public static void main(String[] args) {
+		/*
+		 * Filter all the odd elements 
+		 * Filter all the odd elements and multiply by 2
+		 * Filter all the distinct odd elements and multiply by 2 
+		 * print all the odd elements with stream 
+		 * sum of all the odd elements remove duplicates with stream
+		 * 
+		 */		
+
+//		List <Integer> list = Arrays.asList(1,2,3,4,5,6,7,8);
+//		List <Integer> list = Arrays.asList(6,6,6,6,6,6,6);
+//		List <Integer> list = Arrays.asList(1,1,1,2,2,3,4,5,6,6,6,6);
+//		List<Integer> list = Arrays.asList(10,15,8,49,25,98,32);
+//		Set<Integer> set = new HashSet<>();
+		
+//		List <Integer> ans = list.stream().filter( x -> x%2 == 0 ).collect(Collectors.toList());
+//		List <Integer> ans = list.stream().peek(x -> System.out.println(x)).filter( x -> x%2 == 0 ).map(x -> x * 2 ).collect(Collectors.toList());
+//		List <Integer> ans = list.stream().peek(x -> System.out.println(x)).filter( x -> x%2 == 0 ).distinct().map(x -> x * 2 ).collect(Collectors.toList());		
+//		list.stream().sorted().filter( x -> x%2 == 0 ).forEach(a -> System.out.println("problme + "+ a));
+//		int ans = list.stream().filter( x -> x%2 == 0 ).mapToInt(x -> x ).sum();
+//		list.stream().filter(x -> !set.add(x)).forEach(System.out::println);
+		
+		
+//		System.out.println(ans);
+	}
+}
